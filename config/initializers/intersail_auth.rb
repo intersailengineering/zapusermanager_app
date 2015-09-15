e9b0ad965ce5be6cf742683e451ade554d7593ef
jacopo.beschi <jacopo.beschi@intersail.it>
2015-09-15 11:25:30 +0200

    updated cofig

---
+++ config/initializers/intersail_auth.rb
@@ -0,0 +1,5 @@
+IntersailAuth::Configuration.configure do |config|
+  config.base_url = "http://localhost/RestService"
+  config.login_uri = "/sessions"
+  config.application_token = "fe1ec6b9-cb75-437f-96bc-757d4cb60b8b"
+end
